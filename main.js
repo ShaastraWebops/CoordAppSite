f143bc3b4512f5a2e48ac7e2b445ef0742c52860
Gokulan <gokulan97@gmail.com>
2017-04-12 20:25:28 +0530

    Deadlines added

--- main.js
+++ main.js
@@ -17,129 +17,156 @@ var apps = {
 	"Concept and Design": {
 		"Creative branding and Design Strategist": {
 			name: "Creative branding and Design Strategist",
-			app_name: 'Creative_Branding_and_Design_Strategist_Shaastra2018'
+			app_name: 'Creative_Branding_and_Design_Strategist_Shaastra2018',
+			date: '16.04.2017'
 		},
 		"Creative branding and Design Coordinator": {
 			name: "Creative branding and Design Coordinator",
-			app_name: "Creative_Branding_and_Design_Coordinator_Shaastra2018"
+			app_name: "Creative_Branding_and_Design_Coordinator_Shaastra2018",
+			date: '16.04.2017'
 		},
 		"Creative Ambience Head": {
 			name: "Creative Ambience Head",
-			app_name: "Creative_Ambience_Head_Shaastra2018"
+			app_name: "Creative_Ambience_Head_Shaastra2018",
+			date: '16.04.2017'
 		},
 		"Creative Ambience Coordinator": {
 			name: "Creative Ambience Coordinator",
-			app_name: "Creative_Ambience_Coordinator_Shaastra2018"
+			app_name: "Creative_Ambience_Coordinator_Shaastra2018",
+			date: '16.04.2017'
 		},
 		"VFX and Video Editing Head": {
 			name: "VFX and Video Editing Head",
-			app_name: "VFX_and_Video_Editing_Head_Shaastra2018"
+			app_name: "VFX_and_Video_Editing_Head_Shaastra2018",
+			date: '16.04.2017'
 		},
 		"VFX and Video Editing Coordinator": {
 			name: "VFX and Video Editing Coordinator",
-			app_name: "VFX_and_Video_Editing_Coordinator_Shaastra2018"
+			app_name: "VFX_and_Video_Editing_Coordinator_Shaastra2018",
+			date: '16.04.2017'
 		},
 		"Media and Videography Head": {
 			name: "Media and Videography Head",
-			app_name: "Photography_and_Videography_Head_Shaastra2018"
+			app_name: "Photography_and_Videography_Head_Shaastra2018",
+			date: '16.04.2017'
 		},
 		"Media and Videography Coordinator": {
 			name: "Media and Videography Coordinator",
-			app_name: "Photography_and_Videography_Coordinator_Shaastra2018"
+			app_name: "Photography_and_Videography_Coordinator_Shaastra2018",
+			date: '16.04.2017'
 		}
 	},
 
 	"Envisage":{
 		"Super Coordinator": {
 			name: "Super Coordinator",
-			app_name: "Envisage_Super_Coordinator_Application_Shaastra2018"
+			app_name: "Envisage_Super_Coordinator_Application_Shaastra2018",
+			date: '12.04.2017'
 		},
 		"Tech Ambience": {
 			name: "Tech Ambience",
-			app_name: "Envisage_Coordinator_Application_Shaastra2018"
+			app_name: "Envisage_Coordinator_Application_Shaastra2018",
+			date: '16.04.2017'
 		},
 		"Mentor": {
 			name: "Mentor",
-			app_name: "Envisage_Mentor_Application_Shaastra2018"
+			app_name: "Envisage_Mentor_Application_Shaastra2018",
+			date: '16.04.2017'
 		},
 		"Coordinator Application": {
 			name: "Coordinator Application",
-			app_name: "Envisage_Coordinator_Application_Shaastra2018"
+			app_name: "Envisage_Coordinator_Application_Shaastra2018",
+			date: '16.04.2017'
 		}
 	},
 
 	"Events":{
 		"Events Head": {
 			name: "Events Head",
-			app_name: "Shaastra18_Event_Head_Application"
+			app_name: "Shaastra18_Event_Head_Application",
+			date: '16.04.2017'
 		},
 		"Events Coordinator": {
 			name: "Events Coordinator",
-			app_name: "Shaastra18_Event_Coord_Application"
+			app_name: "Shaastra18_Event_Coord_Application",
+			date: '15.04.2017'
 		},
 	},
 
 	"Evolve": { 
 		"Summit Coordinator": {
 			name: "Summit Coordinator",
-			app_name: "Summit_CoordinatorApplicationQuestions_Shaastra2018"
+			app_name: "Summit_CoordinatorApplicationQuestions_Shaastra2018",
+			date: '16.04.2017'
 		},
 		"Spotlight Coordinator": {
 			name: "Spotlight Coordinator",
-			app_name: "Spotlight_CoordinatorApplicationQuestions_Shaastra2018"
+			app_name: "Spotlight_CoordinatorApplicationQuestions_Shaastra2018",
+			date: '16.04.2017'
 		},
 		"New Department Coordinator": {
 			name: "New Department Coordinator",
-			app_name: "DepartmentX_CoordinatorApplicationQuestions_Shaastra2018"
+			app_name: "DepartmentX_CoordinatorApplicationQuestions_Shaastra2018",
+			date: '16.04.2017'
 		},
 		"Summit Super Coordinator": {
 			name: "Summit Super Coordinator",
-			app_name: "Summit_CoordinatorApplicationQuestions_Shaastra2018"
+			app_name: "Summit_CoordinatorApplicationQuestions_Shaastra2018",
+			date: '14.04.2017'
 		},
 		"Spotlight Super Coordinator": {
 			name: "Spotlight Super Coordinator",
-			app_name: "Spotlight_CoordinatorApplicationQuestions_Shaastra2018"
+			app_name: "Spotlight_CoordinatorApplicationQuestions_Shaastra2018",
+			date: '14.04.2017'
 		},
 		"New Department Super Coordinator": {
 			name: "New Department Super Coordinator",
-			app_name: "DepartmentX_CoordinatorApplicationQuestions_Shaastra2018"
+			app_name: "DepartmentX_CoordinatorApplicationQuestions_Shaastra2018",
+			date: '14.04.2017'
 		}
 	},
 
 	"Finance":{
 		"Finance Coordinator": {
 			name: "Finance Coordinator",
-			app_name: "Financeapplication"
+			app_name: "Financeapplication",
+			date: '16.04.2017'
 		},
 		"Finance Manager" : {
 			name: "Finance Manager",
-			app_name: "Financeapplication"
+			app_name: "Financeapplication",
+			date: '12.04.2017'
 		}
 	},
 	
 	"Operations and Infrastructure Planning":{
 		"Operations Head": {
 			name: "Operations Head",
-			app_name: "O&IP_OperationsHead_Application_Shaastra2018"
+			app_name: "O&IP_OperationsHead_Application_Shaastra2018",
+			date: '13.04.2017'
 		},
 		"Catering Head": {
 			name: "Catering Head",
-			app_name: "O&IP_CateringHead_Application_Shaastra2018"
+			app_name: "O&IP_CateringHead_Application_Shaastra2018",
+			date: '13.04.2017'
 		},
 		"General Arrangements Coordinator": {
 			name: "General Arrangements Coordinator",
-			app_name: "O&IP_GerneralArrangementsCoordinator_Application_Shaastra2018"
+			app_name: "O&IP_GerneralArrangementsCoordinator_Application_Shaastra2018",
+			date: '16.04.2017'
 		},
 		"Catering Coordinator": {
 			name: "Catering Coordinator",
-			app_name: "O&IP_CateringCoordinator_Application_Shaastra2018"
+			app_name: "O&IP_CateringCoordinator_Application_Shaastra2018",
+			date: '16.04.2017'
 		}
 	},
 
 	"Media and Student Relations":{
 		"Coordinator Application": {
 			name: "Coordinator Application",
-			app_name: "M_SR_Coordinator_Application_Shaastra2018"
+			app_name: "M_SR_Coordinator_Application_Shaastra2018",
+			date: '16.04.2017'
 		},
 		/*"Strategist": {
 			name: "Strategist",
@@ -150,92 +177,112 @@ var apps = {
 	"QMS":{
 		"QMS Coordinator": {
 			name: "QMS Coordinator",
-			app_name: "qms_coordinator_application_shaastra2018"
+			app_name: "qms_coordinator_application_shaastra2018",
+			date: '13.04.2017'
 		},
 		"QMS Executive": {
 			name: "QMS Executive",
-			app_name: "qms_executive_application_shaastra2018"
+			app_name: "qms_executive_application_shaastra2018",
+			date: '13.04.2017'
 		}
 	},
 
 	"Shows and Exhibitions":{
 		"Shows Coordinator": {
 			name: "Shows Coordinator",
-			app_name: "Shows"
+			app_name: "Shows",
+			date: '14.04.2017'
 		},
 		"Exhibitions Super Coordinator": {
 			name: "Exhibitions Super Coordinator",
-			app_name: "Exhibitions_Super_Coordinator"
+			app_name: "Exhibitions_Super_Coordinator",
+			date: '14.04.2017'
 		},
 		"Tech and Innovation Fair Super Coordinator": {
 			name: "Tech and Innovation Fair Super Coordinator",
-			app_name: "Tech_n_Innovation_Fair_Super_Coordinator"
+			app_name: "Tech_n_Innovation_Fair_Super_Coordinator",
+			date: '14.04.2017'
 		},
 		"Tech Creation Super Coordinator": {
 			name: "Tech Creation Super Coordinator",
-			app_name: "Techcreation_Super_Coordinator"
+			app_name: "Techcreation_Super_Coordinator",
+			date: '14.04.2017'
 		},
 		"Exhibitions Coordinator": {
 			name: "Exhibitions Coordinator",
-			app_name: "Exhibitions_Coordinator"
+			app_name: "Exhibitions_Coordinator",
+			date: '16.04.2017'
 		},
 		"Tech and Innovation Fair Coordinator": {
 			name: "Tech and Innovation Fair Coordinator",
-			app_name: "Tech_n_Innovation_Fair_Coordinator"
+			app_name: "Tech_n_Innovation_Fair_Coordinator",
+			date: '16.04.2017'
 		},
 		"Tech Creation Coordinator": {
 			name: "Tech Creation Coordinator",
-			app_name: "Techcreation_Coordinator"
+			app_name: "Techcreation_Coordinator",
+			date: '16.04.2017'
 		}
 	},
 
 	"Sponsorship and Public Relations":{
 		"Corporate Relations": {
 			name: "Corporate Relations",
-			app_name: "Corporate_Relations_Spons_Shaastra 2018"
+			app_name: "Corporate_Relations_Spons_Shaastra 2018",
+			date: '14.04.2017'
 		},
 		"PR Executives": {
 			name: "PR Executives",
-			app_name: "PR_Executive_Spons_Shaastra 2018"
+			app_name: "PR_Executive_Spons_Shaastra 2018",
+			date: '18.04.2017'
 		},
 		"Engage and Endaevour": {
 			name: "Engage and Endaevour",
-			app_name: "Engage_Endeavour_Spons_Shaastra 2018"
+			app_name: "Engage_Endeavour_Spons_Shaastra 2018",
+			date: '15.04.2017'
 		}
 	},
 
 	"Webops":{
 		"Frontend Coordinator": {
 			name: "Frontend Coordinator",
-			app_name: "Shaastra18_Webops_Frontend"
+			app_name: "Shaastra18_Webops_Frontend",
+			date: '27.05.2017'
 		},
 		"Backend Coordinator": {
 			name: "Backend Coordinator",
-			app_name: "Shaastra18_Webops_Backend"
+			app_name: "Shaastra18_Webops_Backend",
+			date: '27.05.2017'
 		},
 		"Mobile App Coordinator": {
 			name: "Mobile App Coordinator",
-			app_name: "Shaastra18_Webops_MobileApp"
+			app_name: "Shaastra18_Webops_MobileApp",
+			date: '27.05.2017'
 		},
 		"Software Development Coordinator": {
 			name: "Software Development Coordinator",
-			app_name: "Shaastra18_Webops_SoftDev"
+			app_name: "Shaastra18_Webops_SoftDev",
+			date: '27.05.2017'
 		},
 		"Frontend Super Coordinator": {
 			name: "Frontend Super Coordinator",
-			app_name: "Shaastra18_Webops_Frontend"
+			app_name: "Shaastra18_Webops_Frontend",
+			date: '16.04.2017'
 		},
 		"Backend Super Coordinator": {
 			name: "Backend Super Coordinator",
-			app_name: "Shaastra18_Webops_Backend"
+			app_name: "Shaastra18_Webops_Backend",
+			date: '16.04.2017'
 		},
 		"Mobile App Super Coordinator": {
 			name: "Mobile App Super Coordinator",
-			app_name: "Shaastra18_Webops_MobileApp"
+			app_name: "Shaastra18_Webops_MobileApp",
+			date: '16.04.2017'
 		},
 		"Software Development Super Coordinator": {
 			name: "Software Development Super Coordinator",
-			app_name: "Shaastra18_Webops_SoftDev"
+			app_name: "Shaastra18_Webops_SoftDev",
+			date: '16.04.2017'
 		}
 	},
 	"ICT": {
